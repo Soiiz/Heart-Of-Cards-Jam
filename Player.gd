8c67537d37d9e27a6fef82821a6bda6f8d12eb47
Soiiz <92901022+Soiiz@users.noreply.github.com>
2022-10-28 13:43:39 -0700

    Dash fix

--- Player.gd
+++ Player.gd
@@ -3,8 +3,8 @@ extends KinematicBody2D
 # Movement Variables
 export var speed = 200
 var dash_speed = 0
-export var max_dash_speed = 600
-export var acceleration = 2000
+export var max_dash_speed = 1200
+export var acceleration = 600
 var dash = false;
 var destination = Vector2()
 var dash_movement = Vector2()
@@ -12,10 +12,12 @@ var dash_movement = Vector2()
 export var health = 3
 
 onready var dash_cooldown = $DashCoolDown
+export var dash_delay: float = 5
 
 func _unhandled_input(event):
-	if event.is_action_pressed("Click"):
+	if event.is_action_pressed("Click") && dash_cooldown.is_stopped():
 		dash = true;
+		dash_speed = 400
 		destination = get_global_mouse_position()
 		print("pressed")
 	
@@ -31,9 +33,11 @@ func dash(delta):
 	if dash == false:
 		dash_speed = 0
 	else:
-		dash_speed += acceleration * delta
-		if dash_speed > max_dash_speed:
-			dash_speed = max_dash_speed
+		dash_speed -= acceleration * delta
+		if dash_speed <= 0:
+			dash_speed = 0
+			dash = false
+			dash_cooldown.start(dash_delay)
 	dash_movement = position.direction_to(destination) * dash_speed
 	if position.distance_to(destination) > 5:
 		dash_movement = move_and_slide(dash_movement)
