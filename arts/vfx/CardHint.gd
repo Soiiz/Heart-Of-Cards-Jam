a0776dd2cb03dc074a9d24f428efc68d95671b1f
Sam Feng <fengyuan20000303@gmail.com>
2023-07-01 22:52:35 +0800

    Final Updates

--- arts/vfx/CardHint.gd
+++ arts/vfx/CardHint.gd
@@ -1,9 +1,20 @@
 extends CPUParticles2D
 
 
-func _ready():
-	set_emitting(true)
+var pos = Vector2.ZERO
 
+func _ready():
+	var card_name = get_parent().get_script().get_path().get_slice("/", 3)
+	print("res://arts/cards/%s.png" % card_name)
+	set_texture(load("res://arts/cards/%s.png" % card_name))
+	set_emitting(true)
+	
+	#pos = get_global_position()
+
+func _process(delta):
+	set_global_scale(Vector2(1, 1))
+	set_global_rotation(0)
+	#set_global_position(pos)
 
 func _on_Timer_timeout():
 	queue_free()
